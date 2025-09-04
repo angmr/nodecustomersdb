db8f3572c7e4fb3123efe8d3c008fe3cc9f45377
angmr <angelikir.mit@gmail.com>
2025-09-04 15:35:15 +0300

    Add doc comments in authenticate.js

--- services/authenticate.js
+++ services/authenticate.js
@@ -2,7 +2,19 @@ const jwt = require('jsonwebtoken');
 const user = require('../db/users');
 const bcrypt = require('bcrypt');
 
-// User login
+/**
+ * User login
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @returns {void}
+ * 
+ * Expects JSON body with email and password, extracts them, and looks up the user by email in the database.
+ * The callback in getUserByEmail returns an array of users, even if only one is expected (email is unique).
+ * If found, compares the provided password with the stored hashed password using bcrypt. 
+ * If they match, creates a JSON Web Token (JWT) signed with a secret key from environment variables and sends it in the response.
+ * The response is a JSON object containing the token.
+ * If the user is not found or the password does not match, responds with a 400 status code.
+ */
 const login = (req, res) => {
     // Extract email and password from the request body
     const email = req.body.email;
@@ -26,7 +38,19 @@ const login = (req, res) => {
   });
 }
 
-// User authentication
+/**
+ * User authentication
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @param {Function} next - Callback function to proceed to Express next middleware.
+ * @returns {void}
+ *
+ * Middleware function that checks for a JWT in the Authorization header of the request.
+ * If the token is present, it verifies the token using the secret key from environment variables.
+ * If the token is valid, it calls next() to proceed to the next middleware or route handler.
+ * If the token is missing or invalid, it responds with a 400 status code.
+ * The JWT should be provided in the Authorization header.
+ */
 const authenticate = (req, res, next) => {
     const token = req.header('Authorization');
     if(!token) {
